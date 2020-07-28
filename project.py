eaafb747b3f8691b8a4ea2f845d8205845dbe28b
jsmie <zinslij@stu.otc.school.nz>
2020-07-29 10:15:41 +1200

    Imported old code from python and made changes where suggested

--- project.py
+++ project.py
@@ -1,1 +1,29 @@
+#variables
+#introduction
+print("Welcome to your digital clock simulator")
+
+time = input("""Commands you may use are !time, !temperature, !day, !date, and
+!start_shopping_list
+""")
+
+#commands
+if ("!time"):
+#Time function
+    from datetime import datetime
+    now = datetime.now()
+    print ("%s:%s:%s" % (now.hour,now.minute,now.second))
+#date function
+elif  "!date":
+
+    from datetime import datetime
+    now = datetime.now()
+    print ("%s/%s/%s" % (now.day,now.month,now.year))
+elif t =="start_shopping_list":
+    print("yes")
+
+
+else:
+    print("incorrect command")
+
+
 
