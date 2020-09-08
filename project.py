3dff05463cae842dee0dad31a98cf263d8e577e2
jsmie <zinslij@stu.otc.school.nz>
2020-09-09 10:48:33 +1200

    Add Loop function to listing command to obtain multiple pieces information

--- project.py
+++ project.py
@@ -1,25 +1,34 @@
 #variables
+
 #introduction
 print("Welcome to your digital clock simulator")
 
-time = input("""Commands you may use are !time, !temperature, !day, !date, and
+answer = input("""Commands you may use are !time, !temperature, !day, !date, and
 !start_shopping_list
 """)
 
 #commands
-if ("!time"):
+if ("!time") == answer :
 #Time function
     from datetime import datetime
     now = datetime.now()
-    print ("%s:%s:%s" % (now.hour,now.minute,now.second))
-#date function
-elif  "!date":
+    print("%s:%s:%s" % (now.hour,now.minute,now.second))
 
+#date function
+elif ("!date") == answer :
     from datetime import datetime
     now = datetime.now()
-    print ("%s/%s/%s" % (now.day,now.month,now.year))
-elif t =="start_shopping_list":
-    print("yes")
+    print("%s/%s/%s" % (now.day,now.month,now.year))
+
+#List function
+elif ("!start_shopping_list") == answer :
+ repeat=True
+ while repeat == True:
+   shopping_list = input("Please start your list here and type 'done' when finished").strip().lower()
+if shopping_list == 'done':
+         repeat= False
+else:
+    count = list_exe.count(shopping_list)
 
 
 else:
