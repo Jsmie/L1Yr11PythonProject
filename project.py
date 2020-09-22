5b6e8846db23d9345301646c2645f35c6c76ffd4
jsmie <zinslij@stu.otc.school.nz>
2020-09-22 13:09:27 +1200

    correcting indents to run code and labeling functions inside listing function

--- project.py
+++ project.py
@@ -23,16 +23,24 @@ elif ("!date") == answer :
 #List function
 elif ("!start_shopping_list") == answer :
  repeat=True
+
+
+ #repeating function of list
  while repeat == True:
    shopping_list = input("Please start your list here and type 'done' when finished").strip().lower()
-if shopping_list == 'done':
+
+ if shopping_list == 'done':
          repeat= False
-else:
-    count = list_exe.count(shopping_list)
+ else:
+      count = list_exe.count(shopping_list)
 
 
+  #Double up prevention system on lists
+  if count > 0:
+      print("sorry {} is already on the list".format(shopping_list))
 else:
-    print("incorrect command")
+     print("incorrect command")
+
 
 
 
