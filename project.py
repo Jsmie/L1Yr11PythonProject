a6f36118002ec81f9c727e4f3987d485572f6bf3
jsmie <zinslij@stu.otc.school.nz>
2020-10-19 09:06:21 +1300

    Fix shopping_list infinite loop using guides from codeavengers

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 #variables
-
+list_exe = []
 #introduction
 print("Welcome to your digital clock simulator")
 
@@ -26,23 +26,30 @@ elif ("!start_shopping_list") == answer :
 
 
  #repeating function of list
- while repeat == True:
-   shopping_list = input("Please start your list here and type 'done' when finished").strip().lower()
+repeat = True
+while repeat == True:
+  shopping_list = input("Please enter a item for the list or 'done' to end input: ").strip().lower()
 
- if shopping_list == "done":
-         repeat= False
- else:
+  #Check if user has entered done so that loop can be stopped if so
+  if shopping_list == 'done':
+    repeat = False
+  else:
+    #Check if item is already in list
     count = list_exe.count(shopping_list)
 
-
-  #Double up prevention system on lists
+    #If so, give user error message, otherwise add the item and give success message
     if count > 0:
-      print("sorry {} is already on the list".format(shopping_list))
+      print("Sorry {} is already on the list!".format(shopping_list))
     else:
       list_exe.append(shopping_list)
-      print("{} has been added.".format(shopping_list))
-else:
-     print("incorrect command")
+      print("{} has been added!".format(shopping_list))
+
+#Once user is done, print out their list for them
+print("Your list contains : ")
+
+for shopping_list in list_exe:
+  print(shopping_list.title())
+
 
 
 
